087fa86130da64b0d7d81da3f8fba742412d75e8
patrick valeri <patrick.valeri@free.fr>
2022-01-21 13:40:57 +0100

    Commit master du 21-01-2022

---
+++ footer.inc.php
@@ -0,0 +1,22 @@
+<footer style="display:flex;">
+        <section class="liens_pratiques"style="background-color:rgb(198, 205, 211);display:flex;">
+            <ul>
+                <li>Achats sans soucis</li>
+                <li>Articles à petits prix</li>
+                <li>Paiements sécurisés</li>
+                <li>Livraison Valenciennoise</li>
+            </ul>
+        <section class="liens_generaux">
+            <ul>
+                <li>Accueil</li>
+                <li>Catalogue</li>
+                <li>Questions fréquentes</li>
+                <li>Première visite (page informative pour les premier visiteur)</li>
+                <li>Livraison/Retrait</li>
+                <li>Contact</li>
+            </ul>
+        </section>
+        <section class="legal">
+            <H6>Mentions légales / Conditions générales d'utilisation et de vente</H6>
+        </section>
+    </footer>
---
+++ header.inc.php
@@ -0,0 +1,34 @@
+    <header>
+        <nav class="logo_choix">
+           <div style="display:block;text-align:left;">
+            <div>
+                <span>Prixba</span><image src ="./assets/img/soleil.png" alt="logo_prixba" width="60px" height="60px" ></image>
+               Tous vos besoins à petits prix dans le Valenciennois
+            </div>
+
+            <ul style="display:flex;flex-direction: row;margin:0px 2px 0px 2px ;">
+                <li>Choix et commandes des articles</li>
+                <li>Retraits des articles</li>
+                <li>Paiements des articles</li>
+            </ul>   
+                
+            </div>
+
+
+        </nav>
+        <nav class="cherche_choix">
+            <select style="display:block;align-items: center;">
+                <optgroup></optgroup>
+                <option value="">Catégories</option>
+                <option value="">Bricolage</option>
+                <option value="">Cuisine</option>
+                <option value="">Multimédia</option>
+            </select>
+            <ul style="display:flex;flex-direction: row;margin:0px 2px 0px 2px ;color: linen;">
+                <li>Catalogue</li>
+                <li>Questions fréquentes</li>
+                <li>Connexions</li>
+                <li>Panier</li>
+            </ul> 
+        </nav>
+    </header>
